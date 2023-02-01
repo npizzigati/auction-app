e4b9a59389ae6a62f903850cf39219d889a087aa
Nick Pizzigati <npizzigati@gmail.com>
2023-02-01 11:22:13 -0600

    Move all routes to location under api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :autobids
-  resources :bids
   namespace :api do
     namespace :v1 do
       resources :users
       resources :items
+      resources :bids
+      resources :autobids
     end
   end
 end
